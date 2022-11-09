d49493569d462b0598f85996d271a964ca505860
snakewarhead <snakewarhead@gmail.com>
2022-11-09 22:18:46 +0800

    customize the param of 'email to' when receive from http 2

--- test/emailSend.test.js
+++ test/emailSend.test.js
@@ -2,7 +2,8 @@ const emailSend = require('../libs/emailSend')
 
 describe('email send', () => {
   it('send one', async () => {
-    const res = await emailSend.send('test1', 'hahaha')
+    const res = await emailSend.send('test1', 'hahaha11')
+    // const res = await emailSend.send('test1', 'hahaha', 'xxx@xxx.com,yyy@yyy.com')
     console.log(res)
   })
 })
